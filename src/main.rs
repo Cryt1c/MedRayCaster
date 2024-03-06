21aac709182c0c44b024f941993e9fdd4d6fdbe2
david.peherstorfer <david.peherstorfer@netnode.ch>
2024-03-06 18:27:13 +0100

    Implement rendering 3d texture from vol file

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,93 @@
-extern crate gl;
-extern crate glutin;
-
 use gl::types::*;
+use glutin::window;
+use nalgebra::Matrix4;
+use nalgebra::Vector2;
+use nalgebra::Vector3;
+use nalgebra::Vector4;
+use nalgebra_glm::TVec3;
+use std::f32::consts::PI;
 use std::ffi::CString;
 use std::mem;
 use std::ptr;
 use std::str;
+use three_d_asset::Error;
+use three_d_asset::Texture3D;
+use three_d_asset::TextureData;
+use three_d_asset::VoxelGrid;
 
-// Vertex data
-// static VERTEX_DATA: [GLfloat; 18] = [
-//     // first triangle
-//     0.5, 0.5, 0.0, // top right
-//     0.5, -0.5, 0.0, // bottom right
-//     -0.5, 0.5, 0.0, // top left
-//     // second triangle
-//     0.5, -0.5, 0.0, // bottom right
-//     -0.5, -0.5, 0.0, // bottom left
-//     -0.5, 0.5, 0.0, // top left];
-// ];
-
-// static VERTEX_DATA: [GLfloat; 12] = [
-//     0.5, 0.5, 0.0, // top right
-//     0.5, -0.5, 0.0, // bottom right
-//     -0.5, -0.5, 0.0, // bottom left
-//     -0.5, 0.5, 0.0, // top left
-// ];
-
-// static INDICES: [GLint; 6] = [
-//     0, 1, 3, // first triangle
-//     1, 2, 3, // second triangle
-// ];
-
-static VERTEX_DATA: [GLfloat; 18] = [
-    // positions         // colors
-    0.5, -0.5, 0.0, 1.0, 0.0, 0.0, // bottom right
-    -0.5, -0.5, 0.0, 0.0, 1.0, 0.0, // bottom let
-    0.0, 0.5, 0.0, 0.0, 0.0, 1.0, // top
+static VERTEX_DATA: [GLfloat; 32] = [
+    // positions          // colors           // texture coords
+    0.5, 0.5, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, // top right
+    0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, // bottom right
+    -0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, // bottom let
+    -0.5, 0.5, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, // top left
 ];
+
+static INDICES: [GLuint; 6] = [0, 1, 3, 1, 2, 3];
 
 // Shader sources
 static VS_SRC: &'static str = "
 #version 330
-layout (location = 0) in vec3 position;
-layout (location = 1) in vec3 color;
+uniform mat4 ModelViewProjectionMatrix;
+
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
 
 out vec3 ourColor;
+out vec3 TexCoord;
 
 void main() {
-    gl_Position = vec4(position, 1.0);
-    ourColor = color;
+	gl_Position = vec4(aPos, 1.0);
+	ourColor = aColor;
+	TexCoord = vec3(aTexCoord.x, aTexCoord.y, 0.5);
 }";
 
 static FS_SRC: &'static str = "
 #version 330
+uniform mat4 ViewMatrix;
+uniform mat3 NormalMatrix;
+
+uniform float focal_length;
+uniform float aspect_ratio;
+uniform vec2 viewport_size;
+uniform vec3 ray_origin;
+uniform vec3 top;
+uniform vec3 bottom;
+
+uniform vec3 background_colour;
+uniform vec3 material_colour;
+uniform vec3 light_position;
+
+uniform float step_length;
+uniform float threshold;
+
+uniform sampler3D volume;
+uniform sampler2D jitter;
+
+uniform float gamma;
+
+// Ray
+struct Ray {
+    vec3 origin;
+    vec3 direction;
+};
+
+// Axis-aligned bounding box
+struct AABB {
+    vec3 top;
+    vec3 bottom;
+};
+
 out vec4 out_color;
+
+in vec3 TexCoord;
 in vec3 ourColor;
 
-uniform float scale;
+uniform sampler3D texture1;
 
 void main() {
-    vec3 tempColor = ourColor * scale;
-    out_color = vec4(tempColor, 1.0);
+    out_color = texture(texture1, TexCoord);
 }";
 
 fn compile_shader(src: &str, ty: GLenum) -> GLuint {
@@ -132,6 +159,11 @@ fn link_program(vs: GLuint, fs: GLuint) -> GLuint {
     }
 }
 
+fn load_image(path: &str) -> image::DynamicImage {
+    let img = image::open(path).unwrap();
+    img
+}
+
 fn main() {
     let event_loop = glutin::event_loop::EventLoop::new();
     let window = glutin::window::WindowBuilder::new();
@@ -153,14 +185,42 @@ fn main() {
     let mut vao = 0;
     let mut vbo = 0;
     let mut ebo = 0;
+    let mut texture = 0;
+
+    let TEXTURE_SIZE = 4456448;
+    let mut texture_data: [u8; 4456448] = [255; 4456448];
+    // [
+    //     // First layer (2x2)
+    //     255, 0, 0, 255, // Red pixel
+    //     0, 255, 0, 255, // Green pixel
+    //     0, 0, 255, 255, // Blue pixel
+    //     255, 255, 0, 255, // Yellow pixel
+    //     // Second layer (2x2)
+    //     255, 0, 255, 255, // Magenta pixel
+    //     0, 255, 255, 255, // Cyan pixel
+    //     255, 255, 255, 255, // White pixel
+    //     0, 0, 0, 255, // Black pixel
+    // ];
+    let texture_3d: Texture3D = three_d_asset::io::load(&["examples/assets/Skull.vol"])
+        .unwrap()
+        .deserialize("")
+        .unwrap();
+    let texture_3d_data: TextureData = texture_3d.data;
+    if let TextureData::RU8(data) = texture_3d_data {
+        for (i, &value) in data.iter().enumerate().take(TEXTURE_SIZE) {
+            texture_data[i] = value;
+        }
+    }
 
     unsafe {
         // Create Vertex Array Object
         gl::GenVertexArrays(1, &mut vao);
+        gl::GenBuffers(1, &mut vbo);
+        gl::GenBuffers(1, &mut ebo);
+
         gl::BindVertexArray(vao);
 
         // Create a Vertex Buffer Object and copy the vertex data to it
-        gl::GenBuffers(1, &mut vbo);
         gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
         gl::BufferData(
             gl::ARRAY_BUFFER,
@@ -169,19 +229,13 @@ fn main() {
             gl::STATIC_DRAW,
         );
 
-        // Created an Element Buffer Object
-        // gl::GenBuffers(1, &mut ebo);
-        // gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
-        // gl::BufferData(
-        //     gl::ELEMENT_ARRAY_BUFFER,
-        //     (INDICES.len() * mem::size_of::<GLint>()) as GLsizeiptr,
-        //     mem::transmute(&INDICES[0]),
-        //     gl::STATIC_DRAW,
-        // );
-
-        // Use shader program
-        gl::UseProgram(program);
-        gl::BindFragDataLocation(program, 0, CString::new("out_color").unwrap().as_ptr());
+        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+        gl::BufferData(
+            gl::ELEMENT_ARRAY_BUFFER,
+            (INDICES.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+            mem::transmute(&INDICES[0]),
+            gl::STATIC_DRAW,
+        );
 
         // Specify the layout of the vertex data
         // let pos_attr = gl::GetAttribLocation(program, CString::new("position").unwrap().as_ptr());
@@ -190,19 +244,52 @@ fn main() {
             3,
             gl::FLOAT,
             gl::FALSE,
-            mem::size_of::<GLfloat>() as GLint * 6,
+            mem::size_of::<GLfloat>() as GLint * 8,
             ptr::null(),
         );
         gl::EnableVertexAttribArray(0);
+
         gl::VertexAttribPointer(
             1,
             3,
             gl::FLOAT,
             gl::FALSE,
-            mem::size_of::<GLfloat>() as GLint * 6,
+            mem::size_of::<GLfloat>() as GLint * 8,
             (3 * mem::size_of::<GLfloat>()) as *const _,
         );
         gl::EnableVertexAttribArray(1);
+
+        gl::VertexAttribPointer(
+            2,
+            2,
+            gl::FLOAT,
+            gl::FALSE,
+            mem::size_of::<GLfloat>() as GLint * 8,
+            (6 * mem::size_of::<GLfloat>()) as *const _,
+        );
+        gl::EnableVertexAttribArray(2);
+
+        gl::GenTextures(1, &mut texture);
+        gl::BindTexture(gl::TEXTURE_3D, texture);
+        gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+        gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+        gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as i32);
+        gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as i32);
+        gl::TexParameteri(gl::TEXTURE_3D, gl::TEXTURE_WRAP_R, gl::CLAMP_TO_EDGE as i32);
+
+        gl::TexImage3D(
+            gl::TEXTURE_3D,
+            0,
+            gl::RGB as i32,
+            texture_3d.width as i32,
+            texture_3d.height as i32,
+            texture_3d.depth as i32,
+            0,
+            gl::RED,
+            gl::UNSIGNED_BYTE,
+            texture_data.as_ptr() as *const _,
+        );
+        gl::GenerateMipmap(gl::TEXTURE_3D);
     }
 
     event_loop.run(move |event, _, control_flow| {
@@ -218,8 +305,9 @@ fn main() {
                         gl::DeleteProgram(program);
                         gl::DeleteShader(fs);
                         gl::DeleteShader(vs);
-                        gl::DeleteBuffers(1, &vbo);
                         gl::DeleteVertexArrays(1, &vao);
+                        gl::DeleteBuffers(1, &vbo);
+                        gl::DeleteBuffers(1, &ebo);
                     }
                     *control_flow = ControlFlow::Exit
                 }
@@ -230,17 +318,16 @@ fn main() {
                     // Clear the screen to black
                     gl::ClearColor(0.3, 0.3, 0.3, 1.0);
                     gl::Clear(gl::COLOR_BUFFER_BIT);
-                    let now = std::time::SystemTime::now()
-                        .duration_since(std::time::UNIX_EPOCH)
-                        .unwrap()
-                        .as_secs_f64();
-                    let green_value = (now.sin() / 2.0) + 0.5;
-                    let vertex_color_location =
-                        gl::GetUniformLocation(program, CString::new("scale").unwrap().as_ptr());
-                    gl::Uniform1f(vertex_color_location, green_value as f32);
+                    gl::BindTexture(gl::TEXTURE_3D, texture);
+
+                    // Use shader program
+                    gl::UseProgram(program);
+
+                    // set_uniform_values(program, &gl_window.window());
                     // Draw a triangle from the 3 vertices
                     // gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
-                    gl::DrawArrays(gl::TRIANGLES, 0, 3);
+                    gl::BindVertexArray(vao);
+                    gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, std::ptr::null());
                 }
                 gl_window.swap_buffers().unwrap();
             }
@@ -248,4 +335,122 @@ fn main() {
         }
         gl_window.window().request_redraw();
     });
+}
+trait Uniform {
+    fn set_uniform(&self, location: GLint);
+}
+
+impl Uniform for f32 {
+    fn set_uniform(&self, location: GLint) {
+        unsafe {
+            gl::Uniform1f(location, *self);
+        }
+    }
+}
+
+impl Uniform for i32 {
+    fn set_uniform(&self, location: GLint) {
+        unsafe {
+            gl::Uniform1i(location, *self);
+        }
+    }
+}
+impl Uniform for Matrix4<f32> {
+    fn set_uniform(&self, location: GLint) {
+        unsafe {
+            gl::UniformMatrix4fv(location, 1, gl::FALSE, self.as_ptr());
+        }
+    }
+}
+impl Uniform for Vector2<f32> {
+    fn set_uniform(&self, location: GLint) {
+        unsafe {
+            gl::Uniform2fv(location, 1, self.as_ptr());
+        }
+    }
+}
+impl Uniform for Vector3<f32> {
+    fn set_uniform(&self, location: GLint) {
+        unsafe {
+            gl::Uniform3fv(location, 1, self.as_ptr());
+        }
+    }
+}
+impl Uniform for Vector4<f32> {
+    fn set_uniform(&self, location: GLint) {
+        unsafe {
+            gl::Uniform4fv(location, 1, self.as_ptr());
+        }
+    }
+}
+
+fn set_uniform_value<T: Uniform>(program: GLuint, name: &str, value: T) {
+    unsafe {
+        let location = gl::GetUniformLocation(program, CString::new(name).unwrap().as_ptr());
+        value.set_uniform(location);
+    }
+}
+
+fn set_uniform_values(program: GLuint, window: &window::Window) {
+    let m_dist_exp = -200.0;
+    let m_view_matrix = nalgebra_glm::translate(
+        &nalgebra_glm::identity(),
+        &TVec3::new(0.0, 0.0, -4.0 * (m_dist_exp / 600.0 as f32).exp()),
+    );
+    let m_fov: f32 = 60.0;
+    let m_aspect_ratio = window.inner_size().width as f32 / window.inner_size().height as f32;
+    let fov_radians = m_fov.to_radians();
+    let m_model_view_projection_matrix =
+        nalgebra_glm::perspective(m_aspect_ratio, fov_radians, 0.1, 100.0);
+    // TODO:
+    // let m_normal_matrix: Matrix4<f32> =
+    //     m_view_matrix * m_raycasting_volume.model_matrix.normal_matrix();
+    let m_focal_length = 1.0 / ((PI / 180.0 * m_fov / 2.0).tan());
+    let m_viewport_size = Vector2::new(
+        window.inner_size().width as f32,
+        window.inner_size().height as f32,
+    );
+    // Was Vec3 in original code
+    let m_ray_origin = m_view_matrix.try_inverse().unwrap() * Vector4::new(0.0, 0.0, 0.0, 1.0);
+    // TODO: fix top and bottom
+    let top = m_focal_length * (m_viewport_size.y / 2.0);
+    let bottom = -top;
+    let m_background = Vector3::new(0.0, 0.0, 0.0);
+    let m_light_position = Vector3::new(3.0, 0.0, 3.0);
+    let m_diffuse_material = Vector3::new(1.0, 1.0, 1.0);
+    // Slider in original code
+    let m_step_length = 0.5;
+    // Slider in original code
+    let m_threshold = 0.1;
+    let m_gamma = 2.2;
+    let m_volume = 0.0;
+    let jitter = 1.0;
+
+    set_uniform_value(program, "m_view_matrix", m_view_matrix);
+    set_uniform_value(
+        program,
+        "m_model_view_projection_matrix",
+        m_model_view_projection_matrix,
+    );
+    set_uniform_value(
+        program,
+        "ModelViewProjectionMatrix",
+        m_model_view_projection_matrix,
+    );
+    // TODO:
+    // set_uniform_value(program, "NormalMatrix", m_normal_matrix);
+    set_uniform_value(program, "aspect_ratio", m_aspect_ratio);
+    set_uniform_value(program, "focal_length", m_focal_length);
+    set_uniform_value(program, "viewport_size", m_viewport_size);
+    set_uniform_value(program, "ray_origin", m_ray_origin);
+    set_uniform_value(program, "top", top);
+    set_uniform_value(program, "bottom", bottom);
+    set_uniform_value(program, "background_colour", m_background);
+    set_uniform_value(program, "light_position", m_light_position);
+    set_uniform_value(program, "material_colour", m_diffuse_material);
+    set_uniform_value(program, "step_length", m_step_length);
+    set_uniform_value(program, "threshold", m_threshold);
+    set_uniform_value(program, "gamma", m_gamma);
+    set_uniform_value(program, "volume", m_volume);
+    set_uniform_value(program, "jitter", jitter);
 }
