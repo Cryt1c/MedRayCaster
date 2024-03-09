e8d7020981bf1f05f683d648797b76873825f614
david.peherstorfer <david.peherstorfer@netnode.ch>
2024-03-09 20:29:10 +0100

    Implement rendering of pattern

--- shaders/mip_shader.glsl
+++ shaders/mip_shader.glsl
@@ -1,130 +1,54 @@
-/*
- * Copyright © 2018 Martino Pilia <martino.pilia@gmail.com>
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
- * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 #version 330
 
-out vec4 a_colour;
+in vec4 TexCoord0;
+uniform sampler3D SamplerDataVolume;
 
-uniform mat4 view_matrix;
-
-uniform float focal_length;
-uniform float aspect_ratio;
-uniform vec2 viewport_size;
-uniform vec3 ray_origin;
-uniform vec3 top;
-uniform vec3 bottom;
-
-uniform vec3 background_colour;
-
-uniform float step_length;
-
-uniform sampler3D volume;
-// uniform sampler2D jitter;
-
-uniform float gamma;
-
-// Ray
-struct Ray {
-    vec3 origin;
-    vec3 direction;
-};
-
-// Axis-aligned bounding box
-struct AABB {
-    vec3 top;
-    vec3 bottom;
-};
-
-// Slab method for ray-box intersection
-void ray_box_intersection(Ray ray, AABB box, out float t_0, out float t_1)
-{
-    vec3 direction_inv = 1.0 / ray.direction;
-    vec3 t_top = direction_inv * (box.top - ray.origin);
-    vec3 t_bottom = direction_inv * (box.bottom - ray.origin);
-    vec3 t_min = min(t_top, t_bottom);
-    vec2 t = max(t_min.xx, t_min.yz);
-    t_0 = max(0.0, max(t.x, t.y));
-    vec3 t_max = max(t_top, t_bottom);
-    t = min(t_max.xx, t_max.yz);
-    t_1 = min(t.x, t.y);
-}
-
-// A very simple colour transfer function
-vec4 colour_transfer(float intensity)
-{
-    vec3 high = vec3(1.0, 1.0, 1.0);
-    vec3 low = vec3(0.0, 0.0, 0.0);
-    float alpha = (exp(intensity) - 1.0) / (exp(1.0) - 1.0);
-    return vec4(intensity * high + (1.0 - intensity) * low, alpha);
-}
+out vec4 FragColor;
 
 void main()
 {
-    vec3 ray_direction;
-    ray_direction.xy = 2.0 * gl_FragCoord.xy / viewport_size - 1.0;
-    ray_direction.x *= aspect_ratio;
-    ray_direction.z = -focal_length;
-    ray_direction = (vec4(ray_direction, 0) * view_matrix).xyz;
-
-    float t_0, t_1;
-    Ray casting_ray = Ray(ray_origin, ray_direction);
-    AABB bounding_box = AABB(top, bottom);
-    ray_box_intersection(casting_ray, bounding_box, t_0, t_1);
-
-    vec3 ray_start = (ray_origin + ray_direction * t_0 - bottom) / (top - bottom);
-    vec3 ray_stop = (ray_origin + ray_direction * t_1 - bottom) / (top - bottom);
-
-    vec3 ray = ray_stop - ray_start;
-    float ray_length = length(ray);
-    vec3 step_vector = step_length * ray / ray_length;
-
-    // Random jitter
-    // ray_start += step_vector * texture(jitter, gl_FragCoord.xy / viewport_size).r;
-    ray_start += step_vector * 0.5;
-
-    vec3 position = ray_start;
-
-    float maximum_intensity = 0.0;
-
-    // Ray march until reaching the end of the volume
-    while (ray_length > 0) {
-
-        float intensity = texture(volume, position).r;
-
-        if (intensity > maximum_intensity) {
-            maximum_intensity = intensity;
-        }
-
-        ray_length -= step_length;
-        position += step_vector;
+    vec3 camera = vec3(0.0, 0.0, -1.0);
+    float stepsize = 0.01;
+    vec3 volExtentMin = vec3(0.0, 0.0, 0.0);
+    vec3 volExtentMax = vec3(1.0, 1.0, 1.0);
+    vec4 value;
+    float scalar;
+    // Initialize accumulated color and opacity
+    vec4 dst = vec4(0.0, 0.0, 0.0, 0.0);
+    // Determine volume entry position
+    vec3 position = TexCoord0.xyz;
+    // Compute ray direction
+    vec3 direction = TexCoord0.xyz - camera;
+    direction = normalize(direction);
+    // Loop for ray traversal
+    for (int i = 0; i < 300; i++) // Some large number
+    {
+        // Data access to scalar value in 3D volume texture
+        value = texture(SamplerDataVolume, position);
+        // if (value.r > 0.0)
+        // {
+        //     dst = vec4(0.0, 1.0, 0.0, 0.0);        
+        //     break;
+        // }
+        // else
+        // {
+        //     dst = vec4(1.0, 0.0, 0.0, 0.0);
+        //     break;
+        // }
+        // // Apply transfer function
+        if (value.r == 0.0)
+            continue;
+        vec4 src = vec4(vec3(1.0, 0.0, 0.0), value.r);
+        // Front-to-back compositing
+        dst = (1.0 - dst.a) * src + dst;
+        // Advance ray position along ray direction
+        position += direction * stepsize;
+        // Ray termination: Test if outside volume...
+        vec3 temp1 = sign(position - volExtentMin);
+        vec3 temp2 = sign(volExtentMax - position);
+        float inside = dot(temp1, temp2);
+        if (inside < 3.0)
+            break;
     }
-
-    vec4 colour = colour_transfer(maximum_intensity);
-
-    // Blend background
-    colour.rgb = colour.a * colour.rgb + (1 - colour.a) * pow(background_colour, vec3(gamma)).rgb;
-    colour.a = 1.0;
-
-    // Gamma correction
-    a_colour.rgb = pow(colour.rgb, vec3(1.0 / gamma));
-    a_colour.a = colour.a;
+    FragColor = dst;
 }
--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,18 @@ use nalgebra::Matrix4;
 use nalgebra::Vector2;
 use nalgebra::Vector3;
 use nalgebra::Vector4;
-use nalgebra_glm::TVec3;
-use nalgebra_glm::TVec4;
 use opengl_rs::shader;
-use std::f32::consts::PI;
 use std::ffi::CString;
 use std::mem;
 use std::str;
 use three_d_asset::Texture3D;
 use three_d_asset::TextureData;
 
-// static VERTEX_DATA: [GLfloat; 32] = [
-//     // positions          // colors           // texture coords
-//     0.5, 0.5, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, // top right
-//     0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, // bottom right
-//     -0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, // bottom let
-//     -0.5, 0.5, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, // top left
-// ];
 static VERTEX_DATA: [GLfloat; 24] = [
     -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0, 1.0, -1.0,
     -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0,
 ];
 
-// static VERTEX_DATA: [GLfloat; 9] = [-1.0, -1.0, 0.0, 1.0, -1.0, 0.0, 0.0, 1.0, 0.0];
-
-// static INDICES: [GLuint; 6] = [0, 1, 3, 1, 2, 3];
 static INDICES: [GLuint; 36] = [
     // front
     0, 1, 2, 0, 2, 3, // right
@@ -68,19 +55,7 @@ fn main() {
 
     let texture_size = 4456448;
     let mut texture_data: [u8; 4456448] = [255; 4456448];
-    // [
-    //     // First layer (2x2)
-    //     255, 0, 0, 255, // Red pixel
-    //     0, 255, 0, 255, // Green pixel
-    //     0, 0, 255, 255, // Blue pixel
-    //     255, 255, 0, 255, // Yellow pixel
-    //     // Second layer (2x2)
-    //     255, 0, 255, 255, // Magenta pixel
-    //     0, 255, 255, 255, // Cyan pixel
-    //     255, 255, 255, 255, // White pixel
-    //     0, 0, 0, 255, // Black pixel
-    // ];
-    println!("{}", std::env::current_dir().unwrap().to_str().unwrap());
+
     let texture_3d: Texture3D = three_d_asset::io::load(&["examples/assets/Skull.vol"])
         .unwrap()
         .deserialize("")
@@ -91,6 +66,8 @@ fn main() {
             texture_data[i] = value;
         }
     }
+    println!("Max: {}", texture_data.iter().max().unwrap());
+    println!("Min: {}", texture_data.iter().min().unwrap());
 
     unsafe {
         // Create Vertex Array Object
@@ -185,19 +162,6 @@ fn main() {
                     gl::UseProgram(program);
 
                     set_uniform_values(program, &gl_window.window());
-                    // set_uniform_value(program, "test", 1.0);
-                    // let time = std::time::SystemTime::now()
-                    //     .duration_since(std::time::UNIX_EPOCH)
-                    //     .unwrap()
-                    //     .as_secs_f64();
-                    // let mut modulated_time = (time * 0.5).sin() * 0.5 + 0.5;
-                    // let modulated_time = 0.5;
-                    // println!("Time: {}", modulated_time);
-                    // gl::Uniform1d(
-                    //     gl::GetUniformLocation(program, CString::new("time").unwrap().as_ptr()),
-                    //     modulated_time,
-                    // );
-                    // Draw a triangle from the 3 vertices
                     // gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
                     gl::BindVertexArray(vao);
                     gl::DrawElements(
@@ -206,9 +170,9 @@ fn main() {
                         gl::UNSIGNED_INT,
                         std::ptr::null(),
                     );
-                    // if gl::GetError() != gl::NO_ERROR {
-                    //     println!("Error: {}", gl::GetError());
-                    // }
+                    if gl::GetError() != gl::NO_ERROR {
+                        println!("Error: {}", gl::GetError());
+                    }
                 }
                 gl_window.swap_buffers().unwrap();
             }
@@ -283,52 +247,14 @@ fn set_uniform_value<T: Uniform>(program: GLuint, name: &str, value: T) {
 }
 
 fn set_uniform_values(program: GLuint, window: &window::Window) {
-    let m_dist_exp = -200.0;
-    let m_view_matrix = nalgebra_glm::translate(
-        &nalgebra_glm::identity(),
-        &TVec3::new(0.0, 0.0, -4.0 * (m_dist_exp as f32 / 600.0 as f32).exp()),
-    );
     let m_fov: f32 = 60.0;
     let fov_radians = m_fov.to_radians();
     let m_aspect_ratio = window.inner_size().width as f32 / window.inner_size().height as f32;
     let m_model_view_projection_matrix =
         nalgebra_glm::perspective(fov_radians, m_aspect_ratio, 0.1, 100.0);
-    let m_focal_length = 1.0 / ((PI / 180.0 * m_fov / 2.0).tan());
-    let m_viewport_size = Vector2::new(
-        window.inner_size().width as f32,
-        window.inner_size().height as f32,
-    );
-
-    // Was Vec3 in original code
-    let m_ray_origin_4d = m_view_matrix.try_inverse().unwrap() * TVec4::new(0.0, 0.0, 0.0, 1.0);
-    let m_ray_origin = TVec3::new(m_ray_origin_4d.x, m_ray_origin_4d.y, m_ray_origin_4d.z);
-
-    // TODO: fix top and bottom
-    let top = m_focal_length * (m_viewport_size.y / 2.0);
-    let bottom = -top;
-    let m_background = Vector3::new(0.0, 0.0, 0.0);
-    // Slider in original code
-    let m_step_length = 0.5;
-    // Slider in original code
-    let m_gamma = 2.2;
-    let m_volume = 0.0;
-    // let jitter = 1.0;
-
-    // set_uniform_value(program, "m_view_matrix", m_view_matrix);
     set_uniform_value(
         program,
         "m_model_view_projection_matrix",
         m_model_view_projection_matrix,
     );
-    set_uniform_value(program, "aspect_ratio", m_aspect_ratio);
-    set_uniform_value(program, "focal_length", m_focal_length);
-    set_uniform_value(program, "viewport_size", m_viewport_size);
-    set_uniform_value(program, "ray_origin", m_ray_origin);
-    set_uniform_value(program, "top", top);
-    set_uniform_value(program, "bottom", bottom);
-    set_uniform_value(program, "background_colour", m_background);
-    set_uniform_value(program, "step_length", m_step_length);
-    set_uniform_value(program, "gamma", m_gamma);
-    set_uniform_value(program, "volume", m_volume);
-    // set_uniform_value(program, "jitter", jitter);
 }
