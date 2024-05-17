7e184ff5c5acdfcc55e0b5b7b6a2cf8e2951e5bf
david.peherstorfer <david.peherstorfer@netnode.ch>
2024-05-17 16:42:27 +0200

    Simplify MIP shader and increase quality

--- shaders/mip_shader.glsl
+++ shaders/mip_shader.glsl
@@ -7,12 +7,12 @@ smooth in vec3 vUV;
 uniform sampler3D	volume;
 uniform vec3		camPos;
 
-const int MAX_SAMPLES = 300;	
+const int MAX_SAMPLES = 1000;	
 const vec3 texMin = vec3(0);	
 const vec3 texMax = vec3(1);	
 
 void main() {
-	float step_size = 0.01;
+	float step_size = 0.001;
 	vec3 dataPos = vUV;
 	vec3 geomDir = normalize((vUV-vec3(0.5)) - camPos); 
 	vec3 dirStep = geomDir * step_size; 
@@ -21,10 +21,11 @@ void main() {
 	for (int i = 0; i < MAX_SAMPLES; i++) {
 
         // Sample the volume
-        vec4 value = texture(volume, dataPos);
+        float sample = texture(volume, dataPos).r;
         
-        if (value.r > 0.1) {
-            vFragColor = vec4(value.r, value.r, value.r, 1.0);
+        if (sample > 0.1) {
+	    float scaled_sample = sample * 5;
+            vFragColor.rgba = vec4(scaled_sample, scaled_sample, scaled_sample, 1.0);
             break;
         }
 
