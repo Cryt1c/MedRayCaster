0b6a84d0f1d75d03f091ba6ebea1c00a6dece869
david.peherstorfer <david.peherstorfer@netnode.ch>
2024-05-17 17:30:50 +0200

    Fix MIP shader

--- shaders/mip_shader.glsl
+++ shaders/mip_shader.glsl
@@ -7,7 +7,7 @@ smooth in vec3 vUV;
 uniform sampler3D	volume;
 uniform vec3		camPos;
 
-const int MAX_SAMPLES = 1000;	
+const int MAX_SAMPLES = 2000;	
 const vec3 texMin = vec3(0);	
 const vec3 texMax = vec3(1);	
 
@@ -18,24 +18,20 @@ void main() {
 	vec3 dirStep = geomDir * step_size; 
 	bool stop = false; 
 
+	float max_value = 0.0;
 	for (int i = 0; i < MAX_SAMPLES; i++) {
+		dataPos = dataPos + dirStep;
+		stop = dot(sign(dataPos-texMin),sign(texMax-dataPos)) < 3.0;
 
-        // Sample the volume
-        float sample = texture(volume, dataPos).r;
-        
-        if (sample > 0.1) {
-	    float scaled_sample = sample * 5;
-            vFragColor.rgba = vec4(scaled_sample, scaled_sample, scaled_sample, 1.0);
-            break;
-        }
+		if (stop) {
+			vFragColor.rgba = vec4(max_value, max_value, max_value, max_value);
+			break;
+		}
 
-        // Advance ray position along ray direction
-        dataPos = dataPos + dirStep;
+    float sample = texture(volume, dataPos).r;
 
-        // Ray termination: Test if outside volume...
-	stop = dot(sign(dataPos-texMin),sign(texMax-dataPos)) < 3.0;
-
-	if (stop) 
-		break;
+		if (sample > max_value) {
+	    max_value = sample;
+		}
 	}
 }
