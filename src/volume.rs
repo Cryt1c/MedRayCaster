18126c3775b2da6f38019ad0bdd297009ea722c9
david.peherstorfer <david.peherstorfer@netnode.ch>
2024-05-17 15:42:29 +0200

    Implement mhd file parser and add unit test

--- src/volume.rs
+++ src/volume.rs
@@ -1,8 +1,10 @@
 use byteorder::LittleEndian;
 use byteorder::ReadBytesExt;
+use std::fs::read_to_string;
 use std::fs::File;
 use three_d_asset::{Texture3D, TextureData};
 
+#[derive(Debug, PartialEq)]
 pub struct Dim {
     pub width: i32,
     pub height: i32,
@@ -36,15 +38,10 @@ impl Volume {
             4, 5, 1, 4, 1, 0, // bottom
         ];
 
+        // TODO: Automatically detect file format and use specific loader.
         // let result = Volume::read_vol("examples/assets/Skull.vol");
-        let result = Volume::read_raw(
-            "examples/assets/sinus.raw",
-            Dim {
-                width: 512,
-                height: 512,
-                depth: 333,
-            },
-        );
+        let result = Volume::read_raw("examples/assets/sinus.raw", "examples/assets/sinus.mhd");
+        println!("Texture dimensions: {:?}", result.dimensions);
 
         Volume {
             vertex_data,
@@ -76,7 +73,10 @@ impl Volume {
         }
     }
 
-    pub fn read_raw(file_path: &str, dimensions: Dim) -> Texture {
+    pub fn read_raw(file_path: &str, meta_file_path: &str) -> Texture {
+        let meta_data = read_to_string(meta_file_path).expect("Unable to read MHD file");
+        let dimensions = Volume::parse_meta_data_dim(&meta_data);
+
         let num_elements = dimensions.height * dimensions.width * dimensions.depth;
         let mut file = File::open(file_path).expect("Unable to open RAW file");
         let mut buffer = Vec::with_capacity(num_elements.try_into().unwrap());
@@ -105,6 +105,48 @@ impl Volume {
         let normalized_hu_value = (hu_value as f32 / 4095.0) * 256.0; // Normalize to [0, 256]
         normalized_hu_value as u8
     }
+
+    pub fn parse_meta_data_dim(meta_data: &str) -> Dim {
+        let mut dimensions = Dim {
+            width: 0,
+            height: 0,
+            depth: 0,
+        };
+
+        meta_data.lines().for_each(|line| {
+            let mut data = line.split(" = ");
+
+            match data.next() {
+                Some("NDims") => {
+                    let value = data.next().unwrap().parse::<i32>().unwrap();
+                    // TODO: Use NDims info.
+                    println!("NDims: {}", value);
+                }
+                Some("DimSize") => {
+                    let value = data
+                        .next()
+                        .unwrap()
+                        .split(" ")
+                        .map(|x| x.parse::<i32>().unwrap())
+                        .collect::<Vec<i32>>();
+                    dimensions.width = value[0];
+                    dimensions.height = value[1];
+                    dimensions.depth = value[2];
+                    println!("DimSize: {:?}", value);
+                }
+                Some("ElementSpacing") => {
+                    let value = data.next().unwrap();
+                    // TODO: Use ElementSpacing info.
+                    println!("ElementSpacing: {}", value);
+                }
+                _ => {
+                    println!("Unknown field");
+                }
+            }
+        });
+
+        dimensions
+    }
 }
 
 #[cfg(test)]
@@ -119,7 +161,19 @@ mod test {
             .iter()
             .map(|&x| Volume::normalize_hounsfield_units(x))
             .collect();
-        println!("{:?}", result);
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_parse_dim() {
+        let input = "NDims = 3\nDimSize = 512 512 333\nElementSpacing = 0.402344 0.402344 0.899994";
+        let expected = Dim {
+            width: 512,
+            height: 512,
+            depth: 333,
+        };
+        let result = Volume::parse_meta_data_dim(input);
 
         assert_eq!(expected, result);
     }
