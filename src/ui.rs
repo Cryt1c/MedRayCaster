4e29ee0b5c844d349e7c0f0913516f2b7b1156ae
david.peherstorfer <david.peherstorfer@netnode.ch>
2024-05-28 16:22:01 +0200

    Refactor to render_controls and Scene

--- src/ui.rs
+++ src/ui.rs
@@ -1,7 +1,9 @@
 use egui::{Color32, Response, Ui};
 use egui_plot::{Bar, BarChart, Legend, Plot};
 
-use crate::volume::Volume;
+use crate::{renderer::Scene, shader::ShaderType, volume::Volume};
+
+pub struct UserInterface;
 
 pub struct FrameTimer {
     pub start_time: std::time::Instant,
@@ -9,28 +11,8 @@ pub struct FrameTimer {
     pub fps: f64,
 }
 
-impl FrameTimer {
-    pub fn update_frames_per_second(self: &mut FrameTimer) -> () {
-        let now = std::time::Instant::now();
-        let elapsed = now - self.start_time;
-        if elapsed.as_secs() > 0 {
-            let fps = self.frame_count as f64 / elapsed.as_secs_f64();
-            self.frame_count = 0;
-            self.start_time = now;
-            self.fps = fps;
-        }
-        self.frame_count += 1;
-    }
-}
-
-pub struct UserInterface {
-    pub lower_threshold: u8,
-    pub upper_threshold: u8,
-    pub frame_timer: FrameTimer,
-}
-
 impl UserInterface {
-    pub fn plot_histogram(ui: &mut Ui, volume: &Volume) -> Response {
+    pub fn render_histogram(ui: &mut Ui, volume: &Volume) -> Response {
         let bars = volume
             .histogram
             .iter()
@@ -49,13 +31,61 @@ impl UserInterface {
             .show(ui, |plot_ui| plot_ui.bar_chart(chart))
             .response
     }
+    pub fn render_controls(ui: &mut Ui, scene: &mut Scene) {
+        ui.horizontal(|ui| {
+            ui.spacing_mut().item_spacing.x = 10.0;
+            ui.vertical(|ui| {
+                ui.add(
+                    egui::Slider::new(&mut scene.lower_threshold, 0..=255).text("Lower Threshold"),
+                );
+                ui.add(
+                    egui::Slider::new(&mut scene.upper_threshold, 0..=255).text("Upper Threshold"),
+                );
+                ui.radio_value(
+                    &mut scene.shader_type,
+                    ShaderType::DefaultShader,
+                    "Default shader",
+                );
+                ui.radio_value(&mut scene.shader_type, ShaderType::MipShader, "MIP shader");
+                ui.radio_value(&mut scene.shader_type, ShaderType::AipShader, "AIP shader");
+                ui.label(format!("FPS: {:.2}", scene.frame_timer.fps));
+            });
+            ui.vertical(|ui| {
+                ui.add(
+                    egui::Slider::new(&mut scene.camera.camera_x, -2.5..=2.5).text("Translation X"),
+                );
+                ui.add(
+                    egui::Slider::new(&mut scene.camera.camera_y, -2.5..=2.5).text("Translation Y"),
+                );
+                ui.add(
+                    egui::Slider::new(&mut scene.camera.camera_z, -2.5..=2.5).text("Translation Z"),
+                );
+            });
+            ui.vertical(|ui| {
+                ui.add(
+                    egui::Slider::new(&mut scene.camera.rotation_x, 0.0..=360.0).text("Rotation X"),
+                );
+                ui.add(
+                    egui::Slider::new(&mut scene.camera.rotation_y, 0.0..=360.0).text("Rotation Y"),
+                );
+                ui.add(
+                    egui::Slider::new(&mut scene.camera.rotation_z, 0.0..=360.0).text("Rotation Z"),
+                );
+            });
+        });
+    }
 }
 
-pub struct Camera {
-    pub camera_x: f32,
-    pub camera_y: f32,
-    pub camera_z: f32,
-    pub rotation_x: f32,
-    pub rotation_y: f32,
-    pub rotation_z: f32,
+impl FrameTimer {
+    pub fn update_frames_per_second(self: &mut FrameTimer) -> () {
+        let now = std::time::Instant::now();
+        let elapsed = now - self.start_time;
+        if elapsed.as_secs() > 0 {
+            let fps = self.frame_count as f64 / elapsed.as_secs_f64();
+            self.frame_count = 0;
+            self.start_time = now;
+            self.fps = fps;
+        }
+        self.frame_count += 1;
+    }
 }
